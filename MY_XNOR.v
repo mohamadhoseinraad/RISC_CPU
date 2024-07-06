f24a4ca882dd99e15c97d64c91d809f866c779d4
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2024-07-06 13:39:27 +0330

    update XNOR

--- MY_XNOR.v
+++ MY_XNOR.v
@@ -1,9 +1,7 @@
-module MY_ADD(op1, op2, cin, out, cout);
+module MY_XNOR(op1, op2, out);
     input[7:0] op1, op2;
-    input cin;
     output[7:0] out;
-    output cout;
 
-    assign {cout, out} = op1 + op2 + cin;
+    assign out = ~(op1 ^ op2);
 
 endmodule
