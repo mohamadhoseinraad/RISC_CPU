8f67b9b35a8e9739317fe84ad5228e1f756c055a
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2024-07-06 17:12:00 +0330

    ADD MY_DEC38 , MY_ENC83

---
+++ MY_ENC83.v
@@ -0,0 +1,12 @@
+module MY_ENC83 (in , out);
+
+    input [7:0] in;
+
+    output [2:0] out;
+
+    assign out[0] = in[1] | in[3] | in[5] | in[7];
+    assign out[1] = in[2] | in[3] | in[6] | in[7];
+    assign out[2] = in[4] | in[5] | in[6] | in[7];
+    
+    
+endmodule
